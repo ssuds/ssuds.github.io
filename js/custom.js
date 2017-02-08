580e64b7a7d688081199cc260c98392cd6f8ea2f
ssuds <ssuds@users.noreply.github.com>
2017-02-08 16:10:19 -0600

    implement masonry on engine team page

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,10 @@ $(function(){
   $('.grid').masonry({
     // options
     itemSelector : '.grid-item',
+    stamp : '.stamp',
     columnWidth : 370,
-   gutter: 5, 
-   fitWidth: true
+    gutter: 5, 
+    fitWidth: true
   });
   $('.grid').imagesLoaded().progress( function() {
   $('.grid').masonry('layout');
